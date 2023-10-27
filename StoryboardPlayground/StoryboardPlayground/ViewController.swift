44dbf7059db5dfb33ca44eb50dadcab7470cb390
Kyle Schang <kyle@schang.net>
2023-10-27 14:22:32 -0400

    added the arrays and showing the correct titles

--- StoryboardPlayground/StoryboardPlayground/ViewController.swift
+++ StoryboardPlayground/StoryboardPlayground/ViewController.swift
@@ -17,6 +17,13 @@ class ViewController: UIViewController {
         
     }
 
+    public let titlePercentages:[String] = ["0%","5%","10%","15%","20%","25%","30%","35%","40%","45%","50%","55%","60%","65%","70%","75%","80%","85%","90%","95%","100%"]
+    public let discountPercentages:[Double] = [0.00,0.05,0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50,0.55,0.60,0.65,0.70,0.75,0.80,0.85,0.90,0.95,1.00]
+    public let multiplierPercentages:[Double] = [1.00,0.95,0.90,0.85,0.80,0.75,0.70,0.65,0.60,0.55,0.50,0.45,0.40,0.35,0.30,0.25,0.20,0.15,0.10,0.05,0.00]
+    
+    public var totalDiscountPercentageMultiplier:Double = 0.00
+    
+    
     
     // Creating a PickerView
     @IBOutlet weak var discountPickerView: UIPickerView!
@@ -34,19 +41,18 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     
     // Sets number of items in each component
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 10
-        
+        return titlePercentages.count
     }
     
     // Setting the title for each item in the PickerView
     // Usually make an array of strings an then set the titles from there
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "Test"
+        return titlePercentages[row]
     }
     
     // Sets what you want to happen after you change the selected Row in the PickerView
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print ("Test (\(row))")
+        print ("Test (\(titlePercentages[row])),(\(row))")
     }
     
 }
