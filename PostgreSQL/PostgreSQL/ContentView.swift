0871842cc113c57c3b86157dc39bd555d4b9ef35
Kyle Schang <kyle@schang.net>
2024-08-07 08:55:17 -0400

    working with databases

--- PostgreSQL/PostgreSQL/ContentView.swift
+++ PostgreSQL/PostgreSQL/ContentView.swift
@@ -6,14 +6,36 @@
 //
 
 import SwiftUI
+import PostgresClientKit
+
+     
 
 struct ContentView: View {
+    
+    var configuration = PostgresClientKit.ConnectionConfiguration()
+    
+
+    
+    
     var body: some View {
+    
+        
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+        }
+        .onAppear {
+            do {
+                let connection = try PostgresClientKit.Connection(configuration: configuration)
+                defer { connection.close() }
+                
+                // Perform queries here
+            } catch {
+                print("Error connecting to the database: \(error)")
+            }
         }
         .padding()
     }
